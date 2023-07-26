5285d2b3cf71b94150e49279f099c8fc0d1c0330
Driss <drissaitkassi@gmail.com>
2023-07-26 14:08:09 +0100

    2nd commit

--- src/main/java/com/ea/patientintake/PatientIntakeApplication.java
+++ src/main/java/com/ea/patientintake/PatientIntakeApplication.java
@@ -11,7 +11,7 @@ public class PatientIntakeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PatientIntakeApplication.class, args);
 		try {
-			System.out.println("i made a change here !!!");
+			System.out.println("i made a change here  update !!!");
 			ClinicMain.startConsole();
 		} catch (Throwable e) {
 			throw new RuntimeException(e);
